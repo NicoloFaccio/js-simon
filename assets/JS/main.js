9eb89dc1472b67fa89199cf6dc4edd6a5f95f543
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-18 15:11:58 +0100

    working on the finding the milliseconds for the countdown

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,1 +1,8 @@
 //fruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani! Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+//  -Trovare l'orario da cui  si parte con una variabile.
+//  -Capire da quella come trovare i millisecondi che mi staccano dalle 9:30
+//  -Da li andare ad usare setInterval per creare il conto alla rovescia  utlizzando un ciclo for con il "--"
+
+const day = new Date();
+
+console.log(day)
