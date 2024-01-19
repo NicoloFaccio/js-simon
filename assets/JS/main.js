fd256b127b2a98984616342d877db406f8a80deb
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-19 17:43:01 +0100

    fixed the setInterval and the if

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,6 +3,24 @@
 //  -Capire da quella come trovare i millisecondi che mi staccano dalle 9:30
 //  -Da li andare ad usare setInterval per creare il conto alla rovescia  utlizzando un ciclo for con il "--"
 
-const day = new Date();
+const dayTomorrow = new Date("jan 20, 2024 9:30:00").getTime();
 
-console.log(day)
+console.log(dayTomorrow);
+
+const countdown = setInterval(function(){
+    const dayToday = new Date().getTime()
+
+    let timeMinus = dayTomorrow - dayToday
+
+    let hours = Math.floor(timeMinus % (1000*60*60*24) / (1000*60*60))
+    let minutes = Math.floor(timeMinus % (1000*60*60) / (1000*60))
+    let seconds = Math.floor(timeMinus % (1000*60) / 1000)
+
+    console.log("ore: " + hours, "minuti: " + minutes, "secondi: " + seconds)
+
+    if (timeMinus < 0){
+        clearInterval(countdown)
+    }
+}, 1000)
+
+
